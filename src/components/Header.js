bb5feb0798caea15d0db13abecba3dc02c5b0f3d
Clannis <Dodds1990@gmail.com>
2020-12-30 20:00:27 -0600

    update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,16 @@ import { Component } from "react";
 class Header extends Component {
     render() {
         return (
-            <section>
-                Header
-            </section>
+            <header className="header">
+                <div class="header__text-box">
+                    <h1 class="heading-primary">
+                        <span class="heading-primary--main">James Dodds</span>
+                        <span class="heading-primary--sub">Full Stack Developer</span>
+                    </h1>
+
+                    <a href="#section-tours" class="btn">View Projects</a>
+                </div>
+            </header>
         )
     }
 }
